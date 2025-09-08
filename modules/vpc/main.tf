f0a3bc90fb63fad7271596cbce572aa7c29920f5
pengchao <pengchao.ma2@outlook.com>
2025-09-08 18:24:51 +0800

    modified

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = "us-east-1"
-}
-
 # Create VPC
 resource "aws_vpc" "main" {
   cidr_block           = var.vpc_cidr
