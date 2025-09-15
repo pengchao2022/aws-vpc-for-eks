8ae9535778a7e345fa27c7a85d1f1e393774dc7b
pengchao <pengchao.ma2@outlook.com>
2025-09-15 11:35:44 +0800

    modified again

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -13,6 +13,49 @@ output "private_subnet_ids" {
   value       = aws_subnet.private[*].id
 }
 
+output "bastion_host_public_ip" {
+  description = "Public IP address of the bastion host"
+  value       = aws_instance.bastion.public_ip
+}
+
+output "bastion_host_private_ip" {
+  description = "Private IP address of the bastion host"
+  value       = aws_instance.bastion.private_ip
+}
+
+output "bastion_host_id" {
+  description = "ID of the bastion host instance"
+  value       = aws_instance.bastion.id
+}
+
+output "nat_gateway_public_ip" {
+  description = "Public IP address of the NAT Gateway"
+  value       = aws_eip.nat.public_ip
+}
+
+output "vpc_endpoint_ids" {
+  description = "IDs of the VPC endpoints"
+  value       = [for endpoint in aws_vpc_endpoint.eks : endpoint.id]
+}
+
+output "security_group_ids" {
+  description = "IDs of the security groups"
+  value = {
+    bastion       = aws_security_group.bastion.id
+    vpc_endpoint  = try(aws_security_group.vpc_endpoint[0].id, null)
+  }
+}
+
+output "internet_gateway_id" {
+  description = "ID of the Internet Gateway"
+  value       = aws_internet_gateway.main.id
+}
+
+output "nat_gateway_id" {
+  description = "ID of the NAT Gateway"
+  value       = aws_nat_gateway.main.id
+}
+
 output "public_route_table_id" {
   description = "ID of the public route table"
   value       = aws_route_table.public.id
@@ -23,27 +66,7 @@ output "private_route_table_id" {
   value       = aws_route_table.private.id
 }
 
-output "internet_gateway_id" {
-  description = "ID of the internet gateway"
-  value       = aws_internet_gateway.main.id
-}
-
-output "nat_gateway_id" {
-  description = "ID of the NAT gateway"
-  value       = aws_nat_gateway.main.id
-}
-
-output "bastion_host_public_ip" {
-  description = "Public IP address of the bastion host"
-  value       = aws_instance.bastion.public_ip
-}
-
-output "bastion_host_security_group_id" {
-  description = "ID of the bastion host security group"
-  value       = aws_security_group.bastion.id
-}
-
-output "ssh_key_name" {
-  description = "Name of the SSH key pair created for bastion access"
-  value       = aws_key_pair.bastion.key_name
+output "availability_zones" {
+  description = "List of availability zones used by subnets"
+  value       = var.availability_zones
 }
--- outputs.tf
+++ outputs.tf
@@ -1,18 +1,35 @@
 output "bastion_public_ip" {
-  value = module.development_vpc.bastion_host_public_ip
+  description = "Public IP address of the bastion host"
+  value       = module.development_vpc.bastion_host_public_ip
 }
 
 output "vpc_id" {
-  description = "The ID of the VPC"
+  description = "ID of the VPC"
   value       = module.development_vpc.vpc_id
 }
 
 output "public_subnet_ids" {
-  description = "List of public subnet IDs"
+  description = "IDs of the public subnets"
   value       = module.development_vpc.public_subnet_ids
 }
 
 output "private_subnet_ids" {
-  description = "List of private subnet IDs"
+  description = "IDs of the private subnets"
   value       = module.development_vpc.private_subnet_ids
+}
+
+# 可选：输出其他相关信息
+output "bastion_private_ip" {
+  description = "Private IP address of the bastion host"
+  value       = module.development_vpc.bastion_host_private_ip
+}
+
+output "bastion_instance_id" {
+  description = "ID of the bastion host instance"
+  value       = module.development_vpc.bastion_host_id
+}
+
+output "nat_gateway_ip" {
+  description = "Public IP of the NAT Gateway"
+  value       = module.development_vpc.nat_gateway_public_ip
 }
