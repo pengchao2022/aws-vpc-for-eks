f767e2be9b9815b3fe5e6d34115d896f0f4c6712
pengchao <pengchao.ma2@outlook.com>
2025-09-15 11:48:27 +0800

    modified vpc endpoint

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -33,9 +33,62 @@ output "nat_gateway_public_ip" {
   value       = aws_eip.nat.public_ip
 }
 
+# VPC Endpoints 相关输出
 output "vpc_endpoint_ids" {
-  description = "IDs of the VPC endpoints"
-  value       = [for endpoint in aws_vpc_endpoint.eks : endpoint.id]
+  description = "Map of VPC endpoint IDs by service name"
+  value       = { for endpoint in aws_vpc_endpoint.eks : endpoint.service_name => endpoint.id }
+}
+
+output "vpc_endpoint_arns" {
+  description = "Map of VPC endpoint ARNs by service name"
+  value       = { for endpoint in aws_vpc_endpoint.eks : endpoint.service_name => endpoint.arn }
+}
+
+output "vpc_endpoint_dns_entries" {
+  description = "Map of VPC endpoint DNS entries by service name"
+  value       = { for endpoint in aws_vpc_endpoint.eks : endpoint.service_name => endpoint.dns_entry }
+}
+
+output "vpc_endpoint_service_names" {
+  description = "List of VPC endpoint service names"
+  value       = [for endpoint in aws_vpc_endpoint.eks : endpoint.service_name]
+}
+
+output "vpc_endpoint_types" {
+  description = "Map of VPC endpoint types by service name"
+  value       = { for endpoint in aws_vpc_endpoint.eks : endpoint.service_name => endpoint.vpc_endpoint_type }
+}
+
+output "vpc_endpoint_status" {
+  description = "Map of VPC endpoint status by service name"
+  value       = { for endpoint in aws_vpc_endpoint.eks : endpoint.service_name => endpoint.state }
+}
+
+output "interface_vpc_endpoints" {
+  description = "Map of interface VPC endpoint details"
+  value = { for endpoint in aws_vpc_endpoint.eks :
+    endpoint.service_name => {
+      id                    = endpoint.id
+      dns_entry             = endpoint.dns_entry
+      network_interface_ids = endpoint.network_interface_ids
+      private_dns_enabled   = endpoint.private_dns_enabled
+    } if endpoint.vpc_endpoint_type == "Interface"
+  }
+}
+
+output "gateway_vpc_endpoints" {
+  description = "Map of gateway VPC endpoint details"
+  value = { for endpoint in aws_vpc_endpoint.eks :
+    endpoint.service_name => {
+      id        = endpoint.id
+      dns_entry = endpoint.dns_entry
+    } if endpoint.vpc_endpoint_type == "Gateway"
+  }
+}
+
+output "vpc_endpoint_security_group_id" {
+  description = "ID of the security group for VPC endpoints"
+  value       = try(aws_security_group.vpc_endpoint[0].id, null)
 }
 
 output "security_group_ids" {
@@ -69,4 +122,9 @@ output "private_route_table_id" {
 output "availability_zones" {
   description = "List of availability zones used by subnets"
   value       = var.availability_zones
+}
+
+output "vpc_cidr_block" {
+  description = "CIDR block of the VPC"
+  value       = aws_vpc.main.cidr_block
 }
--- outputs.tf
+++ outputs.tf
@@ -18,7 +18,33 @@ output "private_subnet_ids" {
   value       = module.development_vpc.private_subnet_ids
 }
 
-# 可选：输出其他相关信息
+# VPC Endpoints 输出
+output "vpc_endpoint_ids" {
+  description = "Map of VPC endpoint IDs by service name"
+  value       = module.development_vpc.vpc_endpoint_ids
+}
+
+output "vpc_endpoint_service_names" {
+  description = "List of VPC endpoint service names"
+  value       = module.development_vpc.vpc_endpoint_service_names
+}
+
+output "interface_vpc_endpoints" {
+  description = "Interface VPC endpoint details"
+  value       = module.development_vpc.interface_vpc_endpoints
+}
+
+output "gateway_vpc_endpoints" {
+  description = "Gateway VPC endpoint details"
+  value       = module.development_vpc.gateway_vpc_endpoints
+}
+
+output "vpc_endpoint_security_group_id" {
+  description = "Security group ID for VPC endpoints"
+  value       = module.development_vpc.vpc_endpoint_security_group_id
+}
+
+# 其他可选输出
 output "bastion_private_ip" {
   description = "Private IP address of the bastion host"
   value       = module.development_vpc.bastion_host_private_ip
@@ -32,4 +58,9 @@ output "bastion_instance_id" {
 output "nat_gateway_ip" {
   description = "Public IP of the NAT Gateway"
   value       = module.development_vpc.nat_gateway_public_ip
+}
+
+output "vpc_cidr_block" {
+  description = "CIDR block of the VPC"
+  value       = module.development_vpc.vpc_cidr_block
 }
